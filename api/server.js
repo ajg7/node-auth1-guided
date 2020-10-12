cd3aecc5d63c216849f3ded65b1e0fcd5e6348cc
ajg7 <ajgebara@gmail.com>
2020-10-12 12:55:26 -0500

    made notes

--- api/server.js
+++ api/server.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+
 
 const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router.js");
+const protected = require("../auth/protected-mw");
 
 const server = express();
+
+const sessionConfiguration = {
+    name: "monster", //defaults to sid for the cookie name
+    secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+    cookie: {
+      httpOnly: true, //true means JS can't access the cookie data
+      maxAge: 1000 * 60 * 10, // expires after 10 mins
+      secure: process.env.SECURE_COOKIES || false, // true means send cookies over https only
+    },
+    resave: false, // re-save the session information even if there are no changes
+    saveUninitialized: true, // read about GDPR compliance
+}
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.listen(session(sessionConfiguration))
 
-server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", protected, usersRouter);
 
 server.get("/", (req, res) => {
-  res.json({ api: "up" });
+  res.json({ api: "up", session: req.session });
 });
 
 module.exports = server;
